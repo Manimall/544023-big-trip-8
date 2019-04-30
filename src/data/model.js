264cfc73227aab70085c47856ed521a8561b1fc3
Manimall <gorpunov94@mail.ru>
2019-04-30 20:24:39 +0300

    -method update in model.js

--- src/data/model.js
+++ src/data/model.js
@@ -29,7 +29,7 @@ export class Model {
     this._offers = data;
   }
 
-  update(newPoints) {
-    this._points = newPoints;
-  }
+  // update(newPoints) {
+  //   this._points = newPoints;
+  // }
 }
